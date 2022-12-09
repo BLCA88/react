fad136f37403f3e1f0498b5ca3a1c08281d91446
Jose Antunez <jl.antunez88@gmail.com>
2022-12-09 08:02:54 -0300

    Creando plantilla usuario

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './Navbar/Nav'
 import ItemListContainer from './ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './ItemDetailContainer/ItemDetailContainer'
 import Cart from './Cart/Cart'
+import UserLogin from './User/UserLogin'
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
           <Route path='/product/:id' element= {<ItemDetailContainer />}/>
           <Route path='/categorias/:categorias' element= {<ItemListContainer />} />
           <Route path='/ofertas/:sale' element= {<ItemListContainer />} />
-          <Route path='carrito/finalizar-compra' element={<Cart />} />          
+          <Route path='/carrito/finalizar-compra' element={<Cart />} />
+          <Route path='/user/iniciar-sesion' element={<UserLogin />}  />          
         </Routes>        
       </BrowserRouter>
     </>
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,12 +1,13 @@
+import {Link} from 'react-router-dom'
 const User = () => {
     return (
         <div className="relative">
             <>
-                <button type="button" className="flex rounded-full p-1 text-green-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                <Link to={'/user/iniciar-sesion'} type="button" className="flex rounded-full p-1 text-green-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-11' h-11">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                </button>
+                </Link>
             </>
         </div>
     );
